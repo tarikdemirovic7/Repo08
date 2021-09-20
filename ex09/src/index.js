11937a14465b36fdde53cb84ceb21d7a7c825f12
TA <TA@TAs-iMac.local>
2021-09-20 22:49:47 +0200

    JavaScript project Return Boolean Values

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+function isLess(a, b) {
+    // only change code above this line
+    if (a < b) {
+        // Only change code below this line
+        return true;
+        // only change code above this line
+    } else {
+        // Only change code below this line
+        return false;
+        // only change code above this line
+    }
+    // Only change code below this line
+}
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+// only change code above this line
+module.exports = isLess;
